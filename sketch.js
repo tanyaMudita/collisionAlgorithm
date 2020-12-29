061807f53ef7d354c4f6cffa9e57cce9d0160787
tanyaMudita <76211665+tanyaMudita@users.noreply.github.com>
2020-12-29 16:03:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var movingRect, fixedRect;
+
+
+
+function setup() {
+  createCanvas(800,800);
+  movingRect=  createSprite(400, 200, 50, 80);
+  movingRect.shapeColor="green";
+
+  fixedRect=  createSprite(400,400,80,50)
+  fixedRect.shapeColor="green";
+
+}
+
+function draw() {
+  background(255,255,255);  
+  
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+  
+  console.log(movingRect.x-fixedRect.x);
+
+  if(movingRect.x-fixedRect.x<movingRect.width/2+fixedRect.width/2 &&
+    fixedRect.x-movingRect.x<movingRect.width/2+fixedRect.width/2 &&
+    movingRect.y-fixedRect.y<movingRect.height/2+fixedRect.height/2 &&
+    fixedRect.y-movingRect.y<movingRect.height/2+fixedRect.height/2){
+   
+    fixedRect.shapeColor="red";
+    movingRect.shapeColor="red";
+ 
+  }
+  else{
+    fixedRect.shapeColor="green";
+    movingRect.shapeColor="green";
+  }
+  
+  
+  drawSprites();
+}
